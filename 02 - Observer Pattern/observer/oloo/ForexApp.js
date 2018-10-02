9fc5540ac43feb58cf9d8fce397ebf54d6935c73
jgch88 <radium.laboratories@gmail.com>
2018-10-03 02:20:21 +0800

    figured out how to delegate across multiple object links - just be careful about the init() and registerObserver() order

--- 02 - Observer Pattern/observer/oloo/ForexApp.js
+++ 02 - Observer Pattern/observer/oloo/ForexApp.js
@@ -1,10 +1,25 @@
 // main() equivalent
-
+const consoleLoggerDisplayObserver = require('./ConsoleLoggerDisplayObserver.js');
+const rateChangeDisplayObserver = require('./RateChangeDisplayObserver.js');
 const ForexDataSubject = require('./ForexDataSubject.js');
 
+const ConsoleLoggerDisplay = Object.create(consoleLoggerDisplayObserver);
+ConsoleLoggerDisplay.init();
+const RateChangeDisplay = Object.create(rateChangeDisplayObserver);
+RateChangeDisplay.init();
+
 const ForexStation = Object.create(ForexDataSubject);
-//ForexStation.init();
+ForexStation.init();
 
 
+ForexDataSubject.registerObserver(ConsoleLoggerDisplay);
+ForexDataSubject.registerObserver(RateChangeDisplay);
 
 
+setTimeout(() => {
+  ForexDataSubject.removeObserver(RateChangeDisplay);
+}, 45000);
+
+setTimeout(() => {
+  ForexDataSubject.registerObserver(RateChangeDisplay);
+}, 90000);
--- 02 - Observer Pattern/observer/oloo/ForexDataSubject.js
+++ 02 - Observer Pattern/observer/oloo/ForexDataSubject.js
@@ -1,21 +1,10 @@
 const ForexORama = require('./ForexORama.js');
-const forexORama = Object.create(ForexORama);
-
-const consoleLoggerDisplayObserver = require('./ConsoleLoggerDisplayObserver.js');
-const rateChangeDisplayObserver = require('./RateChangeDisplayObserver.js');
-
 const Subject = require('./Subject.js');
 
+const forexORama = Object.create(ForexORama);
 const ForexDataSubject = Object.create(Subject);
 ForexDataSubject.init();
 forexORama.init(ForexDataSubject);
-
-const ConsoleLoggerDisplay = Object.create(consoleLoggerDisplayObserver);
-ConsoleLoggerDisplay.init();
-ForexDataSubject.registerObserver(ConsoleLoggerDisplay);
-const RateChangeDisplay = Object.create(rateChangeDisplayObserver);
-RateChangeDisplay.init();
-ForexDataSubject.registerObserver(RateChangeDisplay);
 
 ForexDataSubject.ForexORama = forexORama;
 
@@ -29,14 +18,5 @@ ForexDataSubject.exchangeRateChanged = function() {
     currentUSDtoJPYExchangeRate
   });
 }
-
-setTimeout(() => {
-  ForexDataSubject.removeObserver(RateChangeDisplay);
-}, 45000);
-
-setTimeout(() => {
-  ForexDataSubject.registerObserver(RateChangeDisplay);
-}, 90000);
-
 
 module.exports = ForexDataSubject;
