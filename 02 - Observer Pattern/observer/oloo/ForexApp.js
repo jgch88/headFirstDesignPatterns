6fe134a4cf63e32a7eac306d332bb3edd278cb87
jgch88 <radium.laboratories@gmail.com>
2018-10-03 02:50:14 +0800

    redid ForexApp OLOO, note the difference between ForexDataSubject and ForexStation. Reordered code to make flow more obvious

--- 02 - Observer Pattern/observer/oloo/ForexApp.js
+++ 02 - Observer Pattern/observer/oloo/ForexApp.js
@@ -1,29 +1,25 @@
 // main() equivalent
 const consoleLoggerDisplayObserver = require('./ConsoleLoggerDisplayObserver.js');
 const rateChangeDisplayObserver = require('./RateChangeDisplayObserver.js');
-const ForexDataSubject = require('./ForexDataSubject.js');
 
-const ForexORama = require('./ForexORama.js');
-const forexORama = Object.create(ForexORama);
+const ForexDataSubject = require('./ForexDataSubject.js');
 
 const ConsoleLoggerDisplay = Object.create(consoleLoggerDisplayObserver);
 ConsoleLoggerDisplay.init();
 const RateChangeDisplay = Object.create(rateChangeDisplayObserver);
 RateChangeDisplay.init();
 
-const ForexStation = Object.create(ForexDataSubject);
-ForexStation.init();
-
-forexORama.init(ForexStation);
-ForexStation.ForexORama = forexORama;
-
-ForexStation.registerObserver(ConsoleLoggerDisplay);
-ForexStation.registerObserver(RateChangeDisplay);
+ForexDataSubject.registerObserver(ConsoleLoggerDisplay);
+ForexDataSubject.registerObserver(RateChangeDisplay);
 
 setTimeout(() => {
-  ForexStation.removeObserver(RateChangeDisplay);
+  ForexDataSubject.removeObserver(RateChangeDisplay);
 }, 45000);
 
 setTimeout(() => {
-  ForexStation.registerObserver(RateChangeDisplay);
+  ForexDataSubject.registerObserver(RateChangeDisplay);
 }, 90000);
+
+const ForexStation = Object.create(ForexDataSubject);
+ForexStation.init();
+
