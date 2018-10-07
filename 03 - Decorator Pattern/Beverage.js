5fa8262a6e662cbf4e8d7eea520a819b5ce456bd
jgch88 <radium.laboratories@gmail.com>
2018-10-07 23:56:57 +0800

    added naive modification of Beverage class to accommodate extra features

---
+++ 03 - Decorator Pattern/Beverage.js
@@ -0,0 +1,53 @@
+const Beverage = {
+  init() {
+    this.description = '';
+    this.milk = false;
+    this.soy = false;
+    this.mocha = false;
+    this.whip = false;
+  },
+
+  getDescription() {
+
+  },
+
+  cost() {
+
+  },
+
+  hasMilk() {
+
+  },
+
+  setMilk() {
+    
+  },
+
+  hasSoy() {
+
+  },
+
+  setSoy() {
+    
+  },
+
+  hasMocha() {
+
+  },
+
+  setMocha() {
+    
+  },
+
+  hasWhip() {
+
+  },
+
+  setWhip() {
+    
+  },
+  
+}
+
+
+module.exports = Beverage;
